4597b5a6676042d8a3faa34446ff15412b372a39
lnbriglio <lnbriglio@gmail.com>
2020-01-04 23:29:16 -0300

    Relocates tests to automated folder

--- automated-tests/challenge-a.test.js
+++ automated-tests/challenge-a.test.js
@@ -6,4 +6,31 @@ describe("challenge a", () => {
     const results = main.challengeA(dataProvider.getOnboardingTasks(), 6);
     expect(results).toEqual(expect.arrayContaining(["Consulting Onboarding"]));
   });
+
+  test("tasks completed in 20 hours", () => {
+    const results = main.challengeA(dataProvider.getOnboardingTasks(), 20);
+    expect(results).toEqual(
+      expect.arrayContaining([
+        "Consulting Onboarding",
+        "Agile",
+        "Solstie 101",
+        "Mentorship Onboarding"
+      ])
+    );
+  });
+
+  test("tasks completed in 40 hours", () => {
+    const results = main.challengeA(dataProvider.getOnboardingTasks(), 40);
+    expect(results).toEqual(
+      expect.arrayContaining([
+        "Consulting Onboarding",
+        "Agile",
+        "Solstie 101",
+        "Mentorship Onboarding",
+        "CXDD Onboarding",
+        "XP Onboarding",
+        "Engineering Onboarding"
+      ])
+    );
+  });
 });
--- index.test.js
+++ index.test.js
@@ -20,35 +20,4 @@ describe("sample tests", () => {
 
 describe("challenge a", () => {
   //TIP: Call main.challengeA
-  test("tasks completed in 6 hours", () => {
-    const results = main.challengeA(dataProvider.getOnboardingTasks(), 6);
-    expect(results).toEqual(expect.arrayContaining(["Consulting Onboarding"]));
-  });
-
-  test("tasks completed in 20 hours", () => {
-    const results = main.challengeA(dataProvider.getOnboardingTasks(), 20);
-    expect(results).toEqual(
-      expect.arrayContaining([
-        "Consulting Onboarding",
-        "Agile",
-        "Solstie 101",
-        "Mentorship Onboarding"
-      ])
-    );
-  });
-
-  test("tasks completed in 40 hours", () => {
-    const results = main.challengeA(dataProvider.getOnboardingTasks(), 40);
-    expect(results).toEqual(
-      expect.arrayContaining([
-        "Consulting Onboarding",
-        "Agile",
-        "Solstie 101",
-        "Mentorship Onboarding",
-        "CXDD Onboarding",
-        "XP Onboarding",
-        "Engineering Onboarding"
-      ])
-    );
-  });
 });
